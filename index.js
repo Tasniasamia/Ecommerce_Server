844b11a4d63f11769522916993de7d6495e34275
tasniaarhamarham <tasniaarhamarham@gmail.com>
2023-08-06 12:49:58 +0600

    payment

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.port || 6467
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 var cors = require('cors')
+const SSLCommerzPayment = require('sslcommerz-lts')
 app.use(cors())
 app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sx3fx96.mongodb.net/?retryWrites=true&w=majority`;
@@ -16,13 +17,65 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
+const store_id = process.env.DB_STOREID
+const store_passwd = process.env.DB_PASSWORD
+const is_live = false //true for live, false for sandbox
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const database = client.db("Ecommerce");
     const ProductCollection = database.collection("AllProducts2");
+    const OrderCollection = database.collection("Order");
+    const tran_id=new ObjectId().toString();
+app.post('/Order',async(req,res)=>{
+    const orderdata=req.body;
+    const product=await ProductCollection.findOne({_id:new ObjectId(orderdata.productId)});
+    const PriceInt=parseInt(product.price);
+    const price=PriceInt*100
+    
+    console.log(orderdata); 
+    const data = {
+        total_amount: price,
+        currency: orderdata.currency,
+        tran_id: tran_id, // use unique tran_id for each api call
+        success_url: 'http://localhost:3030/success',
+        fail_url: 'http://localhost:3030/fail',
+        cancel_url: 'http://localhost:3030/cancel',
+        ipn_url: 'http://localhost:3030/ipn',
+        shipping_method: 'Courier',
+        product_name: 'Computer.',
+        product_category: 'Electronic',
+        product_profile: 'general',
+        cus_name: 'Customer Name',
+        cus_email: 'customer@example.com',
+        cus_add1: orderdata.address,
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: orderdata.postcode,
+        cus_country: 'Bangladesh',
+        cus_phone: orderdata.phone,
+        cus_fax: '01711111111',
+        ship_name: orderdata.name,
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+    };
+    console.log(data);
+    const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+    sslcz.init(data).then(apiResponse => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL
+        res.send({url:GatewayPageURL})
+        console.log('Redirecting to: ', GatewayPageURL)
+    });
+
+    
+})
     app.get('/Products',async(req,res)=>{
         const result=await ProductCollection.find().toArray();
         console.log("Products", result);
